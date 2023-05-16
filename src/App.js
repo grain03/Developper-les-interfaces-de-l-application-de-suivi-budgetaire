15ed0f2e56a88f264c9902ad6c29e49197c1e8f9
Reda Grain <97684199+grain03@users.noreply.github.com>
2023-05-16 10:07:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,133 +1,40 @@
-import React from "react";
-import "./styles/Form.css";
+import "./App.css";
 import { useState } from "react";
+import Nav from "./componentes/Nav.jsx";
+import Stats from "./componentes/Stats.jsx";
+import Form from "./componentes/Form.jsx";
+import History from "./componentes/History";
 
-function Form({
-  operation,
-  setoperation,
-  setOpenPopup,
-  setIncomes,
-  setexpenses,
-  incomes,
-  expenses,
-  setnewcat,
-  categories,
-}) {
-  const today = new Date();
-  let newcat;
-  console.log(categories);
+function App() {
+  const [operation, setoperation] = useState([]);
+  const [OpenPopup, setOpenPopup] = useState("off");
+  const [incomes, setIncomes] = useState([]);
+  const [expenses, setexpenses] = useState([]);
+  const [categories, setnewcat] = useState([]);
 
-  const [errmessage, setmessage] = useState("");
-
-  const [name, setname] = useState("");
-  const [amount, setamount] = useState(0);
-  const [type, settype] = useState("");
-  const [desc, setdesc] = useState("");
-  const [category, setcategory] = useState("");
-
-  const [date, setdate] = useState(
-    today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear()
-  );
-  let newcategory;
-  let add;
-  function formSubmit(e) {
-    e.preventDefault();
-    if (
-      name == "" ||
-      amount == 0 ||
-      type == "false" ||
-      category == "false" ||
-      desc == ""
-    ) {
-      setmessage("Nb: All inputs must be filled!");
-    } else {
-      setoperation([
-        ...operation,
-        {
-          name: name,
-          amount: amount,
-          type: type,
-          category: category,
-          desc: desc,
-          date: date,
-        },
-      ]);
-      setOpenPopup("off");
-    }
-
-    if (type == "income") {
-      setIncomes([...incomes, { amount: amount }]);
-    }
-    if (type == "expense") {
-      setexpenses([...expenses, { amount: amount }]);
-    }
-  }
+  console.log(incomes);
 
   return (
-    <div className="Form">
-      <form onSubmit={(e) => formSubmit(e)}>
-        <div className="form_header">
-          <h1>Add a new operation</h1>
-          <button onClick={() => setOpenPopup("off")}>X</button>
-        </div>
-        {errmessage !== "" ? <p className="message">{errmessage}</p> : null}
+    <div>
+      <Nav setOpenPopup={setOpenPopup} />
+      <Stats operation={operation} incomes={incomes} expenses={expenses} />
 
-        <input
-          onChange={(e) => setname(e.target.value)}
-          type="text"
-          placeholder="Name this operation"
+      <History operation={operation} />
+      {OpenPopup === "on" && (
+        <Form
+          operation={operation}
+          setoperation={setoperation}
+          setOpenPopup={setOpenPopup}
+          setIncomes={setIncomes}
+          setexpenses={setexpenses}
+          incomes={incomes}
+          expenses={expenses}
+          setnewcat={setnewcat}
+          categories={categories}
         />
-        <input
-          type="number"
-          onChange={(e) => setamount(e.target.value)}
-          placeholder="Amount"
-        />
-        <select onChange={(e) => settype(e.target.value)}>
-          <option value="false" selected disabled>
-            Type
-          </option>
-          <option value="income">income</option>
-          <option value="expense">expense</option>
-        </select>
-        <div className="add_category">
-          <input
-            onChange={(e) => {
-              newcategory = e.target.value;
-              newcategory == "" ? (add = false) : (add = true);
-            }}
-            placeholder="Add New category"
-            type="text"
-          />
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              add !== true
-                ? setmessage("Nb: Category cannot be empty")
-                : setnewcat([...categories, { category: newcategory }]);
-            }}
-          >
-            New
-          </button>
-        </div>
-        <select onChange={(e) => setcategory(e.target.value)}>
-          <option value="false" selected disabled>
-            Category
-          </option>
-          {categories.map((cat) => (
-            <option value={cat.category}>{cat.category}</option>
-          ))}
-        </select>
-        <input
-          type="text"
-          onChange={(e) => setdesc(e.target.value)}
-          placeholder="Description"
-        />
-        <button onClick={formSubmit} className="submite">
-          Add this operation
-        </button>
-      </form>
+      )}
     </div>
   );
 }
 
-export default Form;
+export default App;
